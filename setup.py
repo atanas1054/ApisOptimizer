f8104211d5c5c7bb9e9b727e5d099d653d7fce3f
tk-ops <tk-ops@hq.valoratech.com>
2018-10-24 13:10:14 -0400

    Back to un-capped folder structure; removed ColorLogging from install_requires

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='ApisOptimizer',
+    name='apisoptimizer',
     version='0.1.1',
     description='Artificial bee colony framework for tuning arbitrary functions',
     url='http://github.com/tjkessler/apisoptimizer',
     author='Travis Kessler',
     author_email='travis.j.kessler@gmail.com',
     license='MIT',
-    packages=['ApisOptimizer'],
-    install_requires=['numpy', 'colorlogging'],
+    packages=['apisoptimizer'],
+    install_requires=['numpy'],
     zip_safe=False
 )
