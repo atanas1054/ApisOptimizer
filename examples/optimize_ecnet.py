f39f8ac9595023ac5a9605d898a1e0eb87bb76e7
Travis Kessler <travis.j.kessler@gmail.com>
2019-01-24 17:46:10 -0500

    Updated ECNet opt. script, added DB

--- examples/optimize_ecnet.py
+++ examples/optimize_ecnet.py
@@ -1,19 +1,24 @@
 from apisoptimizer import Colony
 from ecnet.model import MultilayerPerceptron
+import ecnet.data_utils
+import ecnet.error_utils
 import multiprocessing as mp
+import matplotlib.pyplot as plt
 
 
 def optimize_ecnet(param_dict, args):
 
     if args['num_processes'] > 0:
         model = MultilayerPerceptron(
-            id=mp.current_process()._identity[0] % args['num_processes']
+            save_path='./tmp/model_{}/model'.format(
+                mp.current_process()._identity[0] % args['num_processes']
+            )
         )
     else:
         model = MultilayerPerceptron()
 
     df = args['DataFrame']
-    df.create_sets(True, split=[0.7, 0.2, 0.1])
+    df.create_sets(True, split=[0.5, 0.3, 0.2])
     pd = df.package_sets()
 
     model.add_layer(len(pd.learn_x[0]), 'relu')
@@ -28,7 +33,8 @@ def optimize_ecnet(param_dict, args):
         pd.valid_x,
         pd.valid_y,
         learning_rate=param_dict['learning_rate'].value,
-        max_epochs=param_dict['vme'].value
+        max_epochs=param_dict['vme'].value,
+        keep_prob=param_dict['keep_prob'].value
     )
 
     return ecnet.error_utils.calc_rmse(
@@ -41,7 +47,7 @@ if __name__ == '__main__':
 
     num_processes = 8
 
-    df = ecnet.data_utils.DataFrame('my_database.csv')
+    df = ecnet.data_utils.DataFrame('cn_model_v1.0.csv')
 
     args = {
         'DataFrame': df,
@@ -49,18 +55,25 @@ if __name__ == '__main__':
     }
 
     abc = Colony(
-        5,
+        50,
         optimize_ecnet,
         obj_fn_args=args,
-        num_processes=num_processes
+        num_processes=num_processes,
+        log_level='debug',
+        log_dir='./logs'
     )
 
-    abc.add_param('learning_rate', 0.0, 0.5)
+    abc.add_param('learning_rate', 0.0, 1.0)
     abc.add_param('hidden_1', 8, 36)
     abc.add_param('hidden_2', 8, 36)
-    abc.add_param('vme', 500, 25000)
+    abc.add_param('vme', 100, 25000)
+    abc.add_param('keep_prob', 0.001, 1.0)
     abc.initialize()
-    for _ in range(5):
+    ticks = []
+    obj_fn_vals = []
+    for i in range(500):
         abc.search()
-        print(abc.best_fitness)
-        print(abc.best_parameters)
+        ticks.append(i)
+        obj_fn_vals.append(abc.ave_obj_fn_val)
+    plt.plot(ticks, obj_fn_vals)
+    plt.show()
